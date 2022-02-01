f0bd00a2a437215b6057637771cbceb32ab9e7da
Mark <markgabon@hotmail.com>
2022-02-01 17:50:37 -0500

    finished login components, working api post.

--- anywherefitness/src/Components/Login.js
+++ anywherefitness/src/Components/Login.js
@@ -1,33 +1,58 @@
 import React, {useState} from "react";
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 
 function Login()  {
+
 const [cred, setCred] = useState({
     client_name: '',
     password: ''
 })
+const push = useNavigate();
 
+const send = () => {
+    push('/register')
+}
 
 const handleChange = (e) => {
 setCred({
     ...cred,
     [e.target.name]: e.target.value
     })
-    console.log(e.target.value)
+    console.log(cred)
 }
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("this is", e)
+    axios.post('https://anywhere-fitness-07-backend.herokuapp.com/api/auth/clients/register', cred)
+    .then(resp => {
+        console.log(resp.data)
+        localStorage.setItem('token', resp.data.token)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+
 
 return (
     <div>
-        <h1>Login Page</h1>
-    <form>
+        <h1>Login</h1>
+    <form onSubmit={handleSubmit}>
     <div>
-        <label htmlFor="username">Username:</label>
-        <input onChange={handleChange} type="text" id="id" name="username"/>
+        <label htmlFor="client_name">Username: </label>
+        <input onChange={handleChange} type="text" id="id" name="client_name"/>
     </div>
     <div>
-        <label htmlFor="password">Password:</label>
-        <input onChange={handleChange} type="text" id="id" name="password"/>
+        <label htmlFor="password">Password: </label>
+        <input onChange={handleChange} type="password" id="id" name="password"/>
     </div>
+    <button>login</button>
     </form>
+    <h3>Dont have an account?</h3>
+<button onClick={send}>Register now!</button>
     </div>
 )
 }
